2e9166b916b3f0a312a7e3e8d169df128e410235
carlos-java-182 <carlos.villasenor@outlook.com>
2019-10-14 13:06:53 -0500

    Exercises loop

--- src/exercise6Loops/forLoop.java
+++ src/exercise6Loops/forLoop.java
@@ -3,8 +3,32 @@ package exercise6Loops;
 public class forLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Constats declaration
+		
+		
+		//Variable declaration
+		
+		
+		//Objects construction
+		
+		
+		//Create a series of 1-100 by 1
+		
+		for(int i=1; i<100; i++)
+			
+		{System.out.println(i);}
+		
+		
+		for(int i=100; i>1; i--)
+			
+		{System.out.println(i);}
+		
+		for(int i=1; i<100; i+=2)
+			
+		{System.out.println(i);}
+				
 	}
+
 
 }
